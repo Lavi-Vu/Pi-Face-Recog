328724c93bb4d8456c2304e50f873fac95d0f7b4
Lavi-Vu <vunguyen2472001@gmail.com>
2023-11-03 15:07:20 +0700

    first commit

---
+++ build/CMakeFiles/FaceRecognition.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lavi/Face-Recognition-Raspberry-Pi-64-bits/src/TArcface.cpp" "CMakeFiles/FaceRecognition.dir/src/TArcface.cpp.o" "gcc" "CMakeFiles/FaceRecognition.dir/src/TArcface.cpp.o.d"
+  "/home/lavi/Face-Recognition-Raspberry-Pi-64-bits/src/TBlur.cpp" "CMakeFiles/FaceRecognition.dir/src/TBlur.cpp.o" "gcc" "CMakeFiles/FaceRecognition.dir/src/TBlur.cpp.o.d"
+  "/home/lavi/Face-Recognition-Raspberry-Pi-64-bits/src/TLive.cpp" "CMakeFiles/FaceRecognition.dir/src/TLive.cpp.o" "gcc" "CMakeFiles/FaceRecognition.dir/src/TLive.cpp.o.d"
+  "/home/lavi/Face-Recognition-Raspberry-Pi-64-bits/src/TMtCNN.cpp" "CMakeFiles/FaceRecognition.dir/src/TMtCNN.cpp.o" "gcc" "CMakeFiles/FaceRecognition.dir/src/TMtCNN.cpp.o.d"
+  "/home/lavi/Face-Recognition-Raspberry-Pi-64-bits/src/TRetina.cpp" "CMakeFiles/FaceRecognition.dir/src/TRetina.cpp.o" "gcc" "CMakeFiles/FaceRecognition.dir/src/TRetina.cpp.o.d"
+  "/home/lavi/Face-Recognition-Raspberry-Pi-64-bits/src/TWarp.cpp" "CMakeFiles/FaceRecognition.dir/src/TWarp.cpp.o" "gcc" "CMakeFiles/FaceRecognition.dir/src/TWarp.cpp.o.d"
+  "/home/lavi/Face-Recognition-Raspberry-Pi-64-bits/src/main.cpp" "CMakeFiles/FaceRecognition.dir/src/main.cpp.o" "gcc" "CMakeFiles/FaceRecognition.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/FaceRecognition.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FaceRecognition.dir/src/TArcface.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/TArcface.cpp.o.d"
+  "CMakeFiles/FaceRecognition.dir/src/TBlur.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/TBlur.cpp.o.d"
+  "CMakeFiles/FaceRecognition.dir/src/TLive.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/TLive.cpp.o.d"
+  "CMakeFiles/FaceRecognition.dir/src/TMtCNN.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/TMtCNN.cpp.o.d"
+  "CMakeFiles/FaceRecognition.dir/src/TRetina.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/TRetina.cpp.o.d"
+  "CMakeFiles/FaceRecognition.dir/src/TWarp.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/TWarp.cpp.o.d"
+  "CMakeFiles/FaceRecognition.dir/src/main.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/main.cpp.o.d"
+  "FaceRecognition"
+  "FaceRecognition.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FaceRecognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
